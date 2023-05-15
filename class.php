c65c71c1b811db0d73790b0c39842c137b594732
unknown <mainulisraf163@gmail.com>
2023-05-15 15:26:17 +0600

    class decliar

--- class.php
+++ class.php
@@ -1,6 +1,19 @@
 <?php 
 
-echo "first data"."<br/>"."second data";
+echo "first data"."<br/>"."second data"."<br/>";
+class Calculation{
 
+    public $a,$b,$c;
+    
+    function sum(){
 
+        $this->c=$this->a + $this->b;
+        return $this->c;
+    }
+}
+
+$obj=new Calculation();
+$obj->a=10;
+$obj->b=20;
+echo $obj->sum();
 ?>
